8fc886edc0f8171f63d7890c388c98c44c426f7f
lii5a <liisa.tallinn@itcollege.ee>
2016-01-03 14:30:26 +0200

    Kustutasin vana ja ebavajalikku koodi (konsoolimängu). Ka vale vastuse tagasiside on nüüd alert.

--- fizzbuzz/Sisinsusin.java
+++ fizzbuzz/Sisinsusin.java
@@ -1,29 +1,21 @@
 package fizzbuzz;
 
-import com.sun.javafx.scene.control.skin.IntegerFieldSkin;
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-
-import javax.swing.*;
-import java.util.Scanner;
+import javafx.stage.WindowEvent;
 
 /**
  * Created by lii5a on 24/11/15.
  */
 public class Sisinsusin extends Application {
     Stage mangulaud;
-    //StackPane levelid;
     static int arv = (int)(Math.random()*10);
     static int liidetav = (int)(Math.random()*10);
 
@@ -31,29 +23,21 @@ public class Sisinsusin extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         mangulaud = primaryStage;
-        mangulaud.setOnCloseRequest(event -> {System.exit(0); });
+        mangulaud.setOnCloseRequest((WindowEvent event) -> {System.exit(0); });
 
         primaryStage.setTitle("Sisin-Susin");
         seadistamangulaud();
-        //kysisisendit();
-        //tagasisideTubli();
     }
 
-    private void tagasisideTubli() {
-
-        //dialoogiakent õppisin tegema siin: http://code.makery.ch/blog/javafx-dialogs-official/
-
+    private void tagasisideTubli() { //siin meetodis kasutatud dialoogiakent õppisin tegema siin: http://code.makery.ch/blog/javafx-dialogs-official/
 
        Alert tubli = new Alert(Alert.AlertType.INFORMATION);
         tubli.setTitle("Pasunad ja lilled!");
         tubli.setHeaderText("Õige vastus! Said ühe punkti juurde!");
         tubli.showAndWait();
-        seadistamangulaud();
-
-
     }
 
-  /*  private int skoor() {
+  /*  private int skoor() { //proovin siin skoori käima saada.
         int skoor = 0;
         if (tagasisideTubli()) {
             skoor = skoor + 1;
@@ -63,27 +47,24 @@ public class Sisinsusin extends Application {
 
     private void seadistamangulaud() {
         //mangulaud.setResizable(false); //kui see käiku lasta, ei saa akent suure(ma)ks teha.
-        //levelid = new StackPane(); //määran levelite layoutiks stackpane
-        GridPane kastid = new GridPane(); //teen uue layouti kastid
-        //levelid.getChildren().add(kastid); //panen kastid levelite sisse
-        //kastid.setGridLinesVisible(true);
+        GridPane kastid = new GridPane(); //teen uue layouti - kastid, sest seal on hea koordinaate määrata, kujundada
         kastid.setAlignment(Pos.CENTER); //kastivärk on akna keskel
-        kastid.setVgap(10);
+        kastid.setVgap(10); //kastide vahel on nats ruumi
 
-        Scene fbStseen = new Scene(kastid, 350,600); //loon uue stseeni fbStseen, mille layout on levelid (stackpane)
+        Scene fbStseen = new Scene(kastid, 350,600); //loon uue stseeni fbStseen, mille layout on kastid (gridPane)
         mangulaud.setScene(fbStseen);
         mangulaud.show();
+
+        // edasi tulevad tekstid ja numbrid ja nupud
 
         Label genereerinumber = new Label((arvStringiks()));
         genereerinumber.setFont(new Font("Helvetica", 300));
         genereerinumber.setTextFill(Color.web("#FF0000"));
         genereerinumber.setTranslateX(50); //viiskend pikslit paremale
-        //genereerinumber.setTranslateY(-50); //sada pikslit üles
 
         Label kasJagub = new Label("JAGUB KOLME VÕI VIIEGA?");
         kasJagub.setFont(new Font("Helvetica", 20));
         kasJagub.setTranslateY(-50);
-        //kasJagub.setTranslateX(-0);
 
         Button jagubMolemaga = new Button ("Mõlemaga");
         jagubMolemaga.setOnMouseClicked(event -> {
@@ -95,16 +76,13 @@ public class Sisinsusin extends Application {
                         }
 
         }
-        );
-
-        //jagubMolemaga.setTranslateY(-30);
-        //jagubMolemaga.setTranslateX(-100);
+        ); //siin lõppeb nupu "Mõlemaga" sündmus ära.
 
         Button viiega = new Button ("Viiega");
         viiega.setTranslateX(110);
         viiega.setOnMouseClicked(event -> {
                     if (arv%5==0) {
-                        tagasisideTubli(); //todo tulevikus: uus number lihtsalt ja kuhugi skoor tiksuma, pole vahevärki vaja näidata.
+                        tagasisideTubli();
                     }
                     else {
                         tagasisideVale();
@@ -117,10 +95,9 @@ public class Sisinsusin extends Application {
         Button kolmega = new Button ("Kolmega");
         kolmega.setTranslateX(195);
 
-
         kolmega.setOnMouseClicked  (event -> {
                     if (arv%3==0) {
-                        tagasisideTubli(); //todo tulevikus: uus number lihtsalt ja kuhugi skoor tiksuma, pole vahevärki vaja näidata.
+                        tagasisideTubli();
                     }
                     else {
                         tagasisideVale();
@@ -129,12 +106,13 @@ public class Sisinsusin extends Application {
                 }
         );
 
-        Label mittekummagagi = new Label ("MITTE KUMMAGAGI? AGA LIIDA TALLE " +liidetav+"!");
+        Label mittekummagagi = new Label ("MITTE KUMMAGAGI? SIIS LIIDA TALLE " +liidetav+":");
         mittekummagagi.setFont(Font.font("Helvetica",FontWeight.BOLD,14));
 
-
-        TextField tekstikoht = new TextField(); //todo: muuda laiust kuidagi väiksemaks
-        tekstikoht.setPromptText("Kirjuta vastus siia ja vajuta enter/OK");
+        TextField tekstikoht = new TextField();
+        tekstikoht.setPromptText("VASTUS");
+        tekstikoht.setMaxWidth(70);
+        tekstikoht.setTranslateX(95);
 
         Button OK = new Button("OK");
         OK.setTranslateX(110);
@@ -145,18 +123,16 @@ public class Sisinsusin extends Application {
                    double d = Double.parseDouble(tekstikoht.getText()); //selle häki googeldasin siit: http://stackoverflow.com/questions/4753339/convert-textfield-value-to-int
                    int i = (int)d;
 
-                    if (i==arv+liidetav) {
-                        tagasisideTubli(); //todo tulevikus: uus number lihtsalt ja kuhugi skoor tiksuma, pole vahevärki vaja näidata.
-                    }
-                    else {
-                        tagasisideVale();
-                    }
 
-                }
+                       if (i==arv+liidetav) {
+                           tagasisideTubli();
+                       }
+                       else {
+                           tagasisideVale();
+                       }
+
+               }
         );
-
-
-
 
         kastid.add(genereerinumber, 2, 1); //veerg, rida
         kastid.add(kasJagub, 2, 2);
@@ -164,87 +140,17 @@ public class Sisinsusin extends Application {
         kastid.add(viiega, 2,2);
         kastid.add(kolmega, 2,2);
         kastid.add(mittekummagagi, 2,3);
-        //kastid.add(vastuslabel, 2,4);
         kastid.add(tekstikoht, 2,4);
         kastid.add(OK, 2,5);
 
     }
 
     private void tagasisideVale() {
-        GridPane vale = new GridPane();
-        vale.setAlignment(Pos.CENTER);
-        Label oigeVastus = new Label("Vale puha!");
-        Scene valeStseen = new Scene(vale, 350,600);
-        mangulaud.setScene(valeStseen);
-        mangulaud.show();
-        vale.add(oigeVastus,2,1);
+        Alert paha = new Alert(Alert.AlertType.INFORMATION);
+        paha.setTitle("Pahasti!");
+        paha.setHeaderText("See läks nüüd vähe mööda. Kaotasid ühe punkti :(");
+        paha.showAndWait();
     }
-
-    private static void kysisisendit() {
-
-        int liidetav = (int)(Math.random()*10);
-        int tulemus = (arv +liidetav);
-        System.out.println(); //see ja järgmised kuus rida on lihtsalt arendamise eesmärgil. pärast kustutan ära.
-        System.out.println();
-        System.out.println("arv on " + arv);
-        System.out.println("liidetav on " + liidetav);
-        System.out.println("tulemus on " + tulemus);
-        System.out.println();
-
-        System.out.println("Kui see arv jagub viiega, ütle 0. Kui kolmega, ütle 1. Kui mõlemaga, ütle 2. Kui ei jagu kummagagi, liida talle " + liidetav);
-
-        try {
-            Scanner kysimus = new Scanner(System.in);
-            int vastus = kysimus.nextInt();
-
-            if ((arv%15==0) && (vastus==2)){
-                System.out.println("Õige! See arv jagus nii kolme kui ka viiega");
-                kysisisendit();
-            }
-            else if ((arv%5==0)&&(vastus==0)){
-                System.out.println("Õige! See arv jagus viiega.");
-                kysisisendit();
-            }
-
-            else if ((arv%3==0) && (vastus==1)){
-                System.out.println("Õige! See arv jagus kolmega.");
-                kysisisendit();
-            }
-
-            else if (((arv%5!=0) && (arv%3!=0)) && (vastus == tulemus)) {
-                System.out.println("Tubli! Oskad hästi liita!");
-                kysisisendit();
-            }
-
-            else {
-                System.out.println("vale vastus");
-            }
-        } catch (Exception e) {
-            //e.printStackTrace();
-            System.out.println("Kuule ära sisesta mingit jama. Sisesta ikka seda, mida küsitud.");
-            kysisisendit();
-        }
-
-        /*else if ((arv%5==0)&&(vastus!=0)){
-            System.out.println("Läks nihu! See arv jagus ju viiega!");
-            genereerinumbreid(arv);
-            kysisisendit();
-        }
-
-
-        else if ((arv%3==0) && (vastus!=1)){
-            System.out.println("Läks nihu! Jagus ju kolmega!");
-            genereerinumbreid(arv);
-            kysisisendit();
-        }
-
-
-        else if ((arv%5!=0) || (arv%3!=0) && (vastus != arv+liidetav)) {
-            System.out.println("Peaksid liitmist veel nats harjutama");
-        }*/
-
-    }
-
 
     public String arvStringiks() {
         return String.valueOf(arv); //selle koha üle olen päris uhke. guugeldasin silm krõllis, Stackoverflow ei aidanud. IDE aitas.
@@ -253,15 +159,15 @@ public class Sisinsusin extends Application {
 
 
 /*todo:
-mäng objektorienteerituks
 mäng tsüklisse
-võiks skoori pidada
+mäng objektorienteerituks
+skoori pidada ja näidata
 püüa error kinni, et ikka numbreid sisestaks
 
-
+nicetohave:
 nupud ütlevad "sisista" ja "susista" ja "sissussista" + hääl
 võiks ajapiirangu panna
-võiks anda leveleid valida. et kas 10, 100 või 1000 piires.
+võiks anda leveleid valida. et kas 10, 100 või 1000 piires. viia üle järgmisele levelile.
 */
 
 //HEUREKAD
