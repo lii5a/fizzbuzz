836f1e00386a64da83289b618c90126756f4efa1
lii5a <liisa.tallinn@itcollege.ee>
2016-01-09 14:14:16 +0200

    Uue mängu loomise klass

---
+++ fizzbuzz/Uus.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+/**
+ * Created by lii5a on 09/01/16.
+ */
+public class Uus {
+
+    public Uus() {
+        Laud.tekstikoht.setText("");
+        Random.teeArv();
+        Random.teeLiidetav();
+    }
+
+}
